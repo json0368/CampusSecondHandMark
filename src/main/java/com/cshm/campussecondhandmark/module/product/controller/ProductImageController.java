511c7f302663f3a36dca48052eb64b568785c3c5
obsidian <l368574304@163.com>
2026-05-02 11:08:04 +0800

    补充注解

--- src/main/java/com/cshm/campussecondhandmark/module/product/controller/ProductImageController.java
+++ src/main/java/com/cshm/campussecondhandmark/module/product/controller/ProductImageController.java
@@ -3,10 +3,12 @@ package com.cshm.campussecondhandmark.module.product.controller;
 import com.cshm.campussecondhandmark.common.result.Result;
 import com.cshm.campussecondhandmark.module.product.service.ProductImageService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +22,8 @@ public class ProductImageController {
     private ProductImageService productImageService;
 
     @PostMapping("/upload")
-    public Result uploadFile(MultipartFile file) {
+    @ApiOperation("上传商品图片，返回图片URL")
+    public Result uploadFile(@RequestParam MultipartFile file) {
         return Result.success(productImageService.uploadFile(file));
     }
 }
